a8d37209b8a2ebe8002f382bd1507fe4b98a4771
juanpablo0614jp-collab <juanpablo0614jp@gmail.com>
2026-02-14 13:14:32 -0500

    Add background logo to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {/* Logo de fondo */}
+        <div
+          className="fixed inset-0 z-0 pointer-events-none flex items-center justify-center"
+          aria-hidden="true"
+        >
+          <img
+            src="/PageGroup_Logo.png"
+            alt=""
+            className="w-[400px] h-[400px] object-contain opacity-[0.03]"
+          />
+        </div>
+
+        {/* Contenido */}
+        <div className="relative z-10">{children}</div>
+      </body>
     </html>
   );
 }
+
