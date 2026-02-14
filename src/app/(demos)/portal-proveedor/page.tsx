6efafdf84ce61365d8517132af0ca6e8752a3b5f
juanpablo0614jp-collab <juanpablo0614jp@gmail.com>
2026-02-14 17:52:57 -0500

    Add provider portal route and nav link

--- src/app/(demos)/portal-proveedor/page.tsx
+++ src/app/(demos)/portal-proveedor/page.tsx
@@ -1,18 +1,5 @@
-<body>
-  {/* Logo de fondo */}
-  <div
-    className="fixed inset-0 z-0 pointer-events-none flex items-center justify-center"
-    aria-hidden="true"
-  >
-    <img
-      src="/PageGroup_Logo.png"
-      alt=""
-      className="w-[400px] h-[400px] object-contain opacity-[0.03]"
-    />
-  </div>
-
-  {/* Contenido */}
-  <div className="relative z-10">
-    {children}
-  </div>
-</body>
+import DemoLayout from "@/components/DemoLayout";
+import DemoCard from "@/components/DemoCard";
+import UploadArea from "@/components/UploadArea";        // opcional, para la subida
+import AlertsPanel from "@/components/AlertsPanel";       // para las alertas
+import { useDemoStore, DOC_LABELS, ALL_DOCS, REQUIRED_DOCS } from "@/context/DemoStore";
